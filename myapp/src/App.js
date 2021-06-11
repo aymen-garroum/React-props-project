f7034a55df7a12f2327e16459626994b73e19106
Aymen Garroum <aymen170384@gmail.com>
2021-06-11 09:53:40 +0200

    UPDATE

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
 import Profile from "./Profile/Profile"
+import myphoto from './aymen-cropped.png'
 
 function App() {
-  return (
-    <Profile/>
+
+  const handleName = (text)=>{alert(text)}
+
+  return (<div style={{display:"flex", justifyContent:"center"}}>
+          <Profile fullname="Garroum Aymen" bio="Born in 1984" profession="Student in Gomycode" name_show={handleName}><img style={{marginBottom:"3%"}} src={myphoto} alt="FICHIER MANQUANT"/></Profile>
+          </div>
   );
 }
 
--- myapp/src/Profile/Profile.js
+++ myapp/src/Profile/Profile.js
@@ -1,8 +1,26 @@
-import Infodata from './Infodata/Infodata'
-import myphoto from './aymen-cropped.png'
+import PropTypes from 'prop-types';
 
-function profil(){
-    return(<Infodata fullname="Garroum Aymen" bio="Born in 1984" profession="Student in Gomycode"><img src={myphoto} alt="FICHIER MANQUANT"/></Infodata>)
+function profil(people){
+    
+    return( <div style={{display:"flex", flexDirection:"column", width:"30%", border:"solid 1px", borderRadius:"15px"}}>
+            <span style={{marginLeft:"1%"}}>Fullname : {people.fullname}</span> <br/>
+            <span style={{marginLeft:"1%"}}>Bio : {people.bio}</span> <br/>
+            <span style={{marginLeft:"1%"}}>Profession : {people.profession}</span> <br/>      
+            {people.children}
+            <button style={{width:"30%", marginLeft:"35%", marginBottom:"3%"}} onClick={e => people.name_show(people.fullname)}> Show Name</button>
+            </div>)
+}
+
+profil.defaultProps = {
+fullname:"Name not entered",
+bio:"No Bio",
+profession:"Not filled"
+}
+
+profil.propTypes = {
+fullname: PropTypes.string,
+bio: PropTypes.string,
+profession : PropTypes.string
 }
 
 export default profil
